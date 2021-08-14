b77760da139597067e7a1a9bf99da3fbf1fdf313
postolenko <denis.postolenko@gmail.com>
2021-08-14 03:12:03 +0300

    MyCrane

--- scripts.js
+++ scripts.js
@@ -1,6 +1,14 @@
 $(document).ready(function(){
     changeContainer($(window).width());
     $(window).resize(function(){ changeContainer($(window).width()); });
+
+    $("[data-dropdown-btn]").on("click", function(e) {
+    	dropdownBox= $(this).attr("data-dropdown-btn");
+    	// $("[data-dropdown = '"+dropdownBox+"']").slideDown(300);
+    	$("[data-dropdown = '"+dropdownBox+"']").addClass("active");
+    	$(this).remove();
+    });
+
 });
 
 function changeContainer(_width){
