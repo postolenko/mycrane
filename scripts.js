9f0c68916025b00e2006b98c2fa660410682bd9c
postolenko <denis.postolenko@gmail.com>
2021-11-04 15:15:12 +0200

    BD

--- scripts.js
+++ scripts.js
@@ -18,6 +18,39 @@ $(document).ready(function(){
     	}
     });
 
+    $(document).on("mouseup", function(e) {
+        e.preventDefault();
+        hide_element = $(".input_wrapp_2");
+        if (!hide_element.is(e.target)
+          && hide_element.has(e.target).length === 0) {
+            $(".input_wrapp_2").each(function() {
+                if( $(this).find("input").val() == "" || e.keyCode === 69) {
+                    $(this).find("input").removeClass("active");
+                }
+                console.log(!/e/gi.test($(this).find("input").val()));
+            });
+        }
+    });
+
+    $(document).on("mouseup", function(e) {
+        e.preventDefault();
+        hide_element = $(".input_wrapp");
+        if (!hide_element.is(e.target)
+          && hide_element.has(e.target).length === 0) {
+            $(".input_wrapp").each(function() {
+                if( $(this).find("input").val() == "") {
+                    $(this).find("input").removeClass("active");
+                }
+            });
+        }
+    });
+
+    $(".input_wrapp_2 input").on("input", function(e) {    
+        if($(this).val() != "" || e.keyCode === 69 || $(this).val().includes('e')) {          
+            $(this).addClass("active");
+        }
+    });
+
 });
 
 function changeContainer(_width){
